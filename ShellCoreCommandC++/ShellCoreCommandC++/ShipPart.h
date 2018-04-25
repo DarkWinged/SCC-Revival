9bb3dc2b8ab708626f00dea4e45a85ea47a2d69e
James Levi Rogers <James.Levi.Rogers@gmail.com>
2018-04-25 00:10:03 -0400

    Moved project location

---
+++ ShellCoreCommandC++/ShellCoreCommandC++/ShipPart.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Image.h"
+
+class ShipPart {
+
+public:
+	ShipPart(int x, int y, int rot, bool horizontalflip, Image *img);
+	~ShipPart();
+
+	int xPos;
+	int yPos;
+
+	int Rotation;
+	bool HorizontallyFlipped;
+
+	Image *partImage;
+};
