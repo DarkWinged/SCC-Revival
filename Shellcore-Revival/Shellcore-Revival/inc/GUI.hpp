c51cb5a04200d34e4d789da45812813f9d484b68
James Levi Rogers <James.Levi.Rogers@gmail.com>
2018-05-10 14:38:14 -0400

    Moved example to GUI class

--- Shellcore-Revival/Shellcore-Revival/inc/GUI.hpp
+++ Shellcore-Revival/Shellcore-Revival/inc/GUI.hpp
@@ -1,4 +1,8 @@
 #pragma once
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
 
 namespace SCC_R {
 	namespace Control {
@@ -8,6 +12,15 @@ namespace SCC_R {
 		public:
 			GUI();
 			~GUI();
+			bool init();
+			int loop();
+
+			void framebuffer_size_callback(int width, int height);
+			void processInput();
+
+			const unsigned int SCR_WIDTH = 960;
+			const unsigned int SCR_HEIGHT = 540;
+			GLFWwindow* window;
 		};
 	}
 }
